ba5cc67f18ffe15f681ab972bc637cd32a7f2247
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2023-12-22 17:05:20 +0900

    first push

---
+++ Libra/LibraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraApp.swift
+//  Libra
+//
+//  Created by 鶴見駿 on 2023/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LibraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

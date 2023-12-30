1d282ca8b6ef763197fa9660b7ed33a25b387c58
orukunnn <orukunnn@orukunnnnoMacBook-Air.local>
2023-12-30 11:59:41 +0900

    feat: UIの大まかな形を作成、セクションを個人所有と団体所有に分断、検索欄を上部に追加

---
+++ Libra/BookDetailView.swift
@@ -0,0 +1,24 @@
+//
+//  BookDetailView.swift
+//  Libra
+//
+//  Created by orukunnn on 2023/12/30.
+//
+
+import SwiftUI
+
+struct BookDetailView: View {
+    let book: Book
+    
+    var body: some View {
+        VStack{
+            Text("test")
+        }
+    }
+}
+//
+//struct BookDetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BookDetailView()
+//    }
+//}
--- Libra/LibraApp.swift
+++ Libra/LibraApp.swift
@@ -6,13 +6,12 @@
 //
 
 import SwiftUI
-import FirebaseCore
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+//    FirebaseApp.configure()
 
     return true
   }
@@ -26,7 +25,8 @@ struct YourApp: App {
   var body: some Scene {
     WindowGroup {
       NavigationView {
-        ContentView()
+//        ContentView()
+          MainScreenView()
       }
     }
   }
